b31b1c67b089fb00af61452ef3f8f0959c54e403
ming@zmeet <jiaozhu@abcpen.com>
2022-11-28 13:45:55 +0800

    translate sdk and demo

--- python/translate_demo.py
+++ python/translate_demo.py
@@ -24,8 +24,8 @@ async def main():
     args = parser.parse_args()
     
     ## 下面的app_id 和api_key仅供测试使用，生产环境请向商务申请(手机：18605811078, 邮箱：jiaozhu@abcpen.com)
-    app_id = "test1"
-    api_key = "2258ACC4-199B-4DCB-B6F3-C2485C63E85A"
+    app_id = ""
+    api_key = ""
     if (len(app_id)<=0 or len(api_key)<=0):
         print("Please apply appid and appsecret, demo will exit now")
         sys.exit(1)
