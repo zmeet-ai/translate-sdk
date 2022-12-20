d9c114e676cb25616e92dcd33764131b00da9cbe
ming@zmeet <jiaozhu@abcpen.com>
2022-12-21 00:38:53 +0800

    update url

--- python/translate_demo.py
+++ python/translate_demo.py
@@ -17,15 +17,15 @@ async def main():
     parser = argparse.ArgumentParser(description="Translate sdk demo",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-u', '--url', type=str, metavar='URL',
-                        help='server url', default='ai.abcpen.com')
+                        help='server url', default='translate.abcpen.com')
     parser.add_argument('-l', '--log_path', type=str, metavar='LOG',
                         help='log file path', default='translate_res.log')
 
     args = parser.parse_args()
     
     ## 下面的app_id 和api_key仅供测试使用，生产环境请向商务申请(手机：18605811078, 邮箱：jiaozhu@abcpen.com)
-    app_id = ""
-    api_key = ""
+    app_id = "test1"
+    api_key = "2258ACC4-199B-4DCB-B6F3-C2485C63E85A"
     if (len(app_id)<=0 or len(api_key)<=0):
         print("Please apply appid and appsecret, demo will exit now")
         sys.exit(1)
